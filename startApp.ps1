33fc87d517a6436edf8785da62df038c5684b988
DenimHorton <denim_horton@hotmail.com>
2022-08-07 14:41:00 -0600

    Added in content

--- startApp.ps1
+++ startApp.ps1
@@ -1,6 +1,13 @@
 #startApp
 source ./WorkoutPlannerVenv/Scripts/activate
 
-./buildDevEnv.ps1 
+py -m pip install --upgrade pip 
+py -m pip uninstall -y Pillow PIL  
+
+py -m pip install -r ./WorkoutPlannerApp/requirements.txt
+
+py ./WorkoutPlannerApp/manage.py migrate
+py ./WorkoutPlannerApp/manage.py makemigrations 
+py ./WorkoutPlannerApp/manage.py migrate
 
 py ./WorkoutPlannerApp/manage.py runserver
